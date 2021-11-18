be6a7c82c29e94e15b9449f5a61e54346e671ce1
Victor Medina <vmedina@ucol.mx>
2021-11-17 18:44:10 -0600

    Iniciamos con vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,333 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bienvenido a Vuetify 2021
+        </h1>
+
+        <v-alert
+          outlined
+          prominent
+          text
+          type="success"
+        >Nueva versión de educ</v-alert>
+
+        <p class="subheading font-weight-regular">
+          <v-badge></v-badge>
+        </p>
+
+        <p class="subheading font-weight-regular">
+          <v-btn
+            elevation="2"
+            large
+            outlined
+            raised
+            tile
+            x-large
+          >clic aquí</v-btn>
+        </p>
+
+
+
+         <v-card
+            class="mx-auto"
+            max-width="344"
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  OVERLINE
+                </div>
+                <v-list-item-title class="text-h5 mb-1">
+                  Headline 5
+                </v-list-item-title>
+                <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-avatar
+                tile
+                size="80"
+                color="grey"
+              ></v-list-item-avatar>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-btn
+                outlined
+                rounded
+                text
+              >
+                Button
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+
+
+          <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-card-text>
+                <div>Word of the Day</div>
+                <p class="text-h4 text--primary">
+                  el·ee·mos·y·nar·y
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                  relating to or dependent on charity; charitable.<br>
+                  "an eleemosynary educational institution."
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="teal accent-4"
+                  @click="reveal = true"
+                >
+                  Learn More
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <v-card
+                  v-if="reveal"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%;"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="text-h4 text--primary">
+                      Origin
+                    </p>
+                    <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+                  </v-card-text>
+                  <v-card-actions class="pt-0">
+                    <v-btn
+                      text
+                      color="teal accent-4"
+                      @click="reveal = false"
+                    >
+                      Close
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+
+
+
+
+
+        <v-container>
+          <v-row justify="space-around">
+            <v-card width="400">
+              <v-img
+                height="200px"
+                src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+              >
+                <v-app-bar
+                  flat
+                  color="rgba(0, 0, 0, 0)"
+                >
+                  <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+
+                  <v-toolbar-title class="text-h6 white--text pl-0">
+                    Messages
+                  </v-toolbar-title>
+
+                  <v-spacer></v-spacer>
+
+                  <v-btn
+                    color="white"
+                    icon
+                  >
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </v-app-bar>
+
+                <v-card-title class="white--text mt-8">
+                  <v-avatar size="56">
+                    <img
+                      alt="user"
+                      src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+                    >
+                  </v-avatar>
+                  <p class="ml-3">
+                    John Doe
+                  </p>
+                </v-card-title>
+              </v-img>
+
+              <v-card-text>
+                <div class="font-weight-bold ml-8 mb-2">
+                  Today
+                </div>
+
+                <v-timeline
+                  align-top
+                  dense
+                >
+                  <v-timeline-item
+                    v-for="message in messages"
+                    :key="message.time"
+                    :color="message.color"
+                    small
+                  >
+                    <div>
+                      <div class="font-weight-normal">
+                        <strong>{{ message.from }}</strong> @{{ message.time }}
+                      </div>
+                      <div>{{ message.message }}</div>
+                    </div>
+                  </v-timeline-item>
+                </v-timeline>
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-container>    
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
